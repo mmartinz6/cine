0deb67901211450bc10588e36013668970d3b3ec
mmartinz6 <madmartinez@gmail.com>
2025-08-26 14:40:35 -0600

    css cambio

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,67 @@
-import { getMovies } from "../services/servicesMovies.js"; 
+import { getMovies, postMovies } from "../services/servicesMovies.js"; 
 
-const 
+
+const poster = document.getElementById("poster");
+const nameMovie = document.getElementById("nameMovie");
+const anhoLanzamiento = document.getElementById("anhoLanzamiento");
+const movGenero = document.getElementById("movGenero");
+const btnGuardarMovie = document.getElementById("btnGuardarMovie");
+const mostrarPeli = document.getElementById("mostrarPeli");
+
+btnGuardarMovie.addEventListener("click", async function() {
+
+    if(!poster.value.trim() || !nameMovie.value.trim() || !anhoLanzamiento.value.trim() || !movGenero.value.trim()){
+        alert("❌Complete todos los campos❌")
+        return;
+    }
+
+    const movie={
+        poster:poster.value,
+        titulo:nameMovie.value,
+        anho:anhoLanzamiento.value,
+        genero:movGenero.value
+    };
+    
+    const respuestaConfirmada = await postMovies(movie);
+
+    console.log(respuestaConfirmada);
+    
+})
+
+//MUESTRA PELICULAS
+async function datosMovies() {
+
+    const datosMoviesRecibidos = await getMovies();
+
+    //map filter
+    datosMoviesRecibidos.map(movie=>{
+
+        let contenedorMovie = document.createElement("div");
+        contenedorMovie.classList.add("movieDiv");
+
+        let imgPoster = document.createElement("img");
+        imgPoster.src = movie.poster;
+        imgPoster.classList.add("moviePoster");
+
+        let titulo = document.createElement("h2");
+        titulo.textContent = movie.titulo;
+        titulo.classList.add("movieTitulo");
+
+        let genero = document.createElement("h3")
+        genero.textContent = movie.genero;
+        genero.classList.add("movieGenero");
+
+        let anho = document.createElement("h3")
+        anho.textContent = movie.anhoLanzamiento;
+        anho.classList.add("movieAhno");
+
+        mostrarPeli.appendChild(imgPoster);
+        mostrarPeli.appendChild(titulo);
+        mostrarPeli.appendChild(genero);
+        mostrarPeli.appendChild(anho);
+
+        mostrarPeli.appendChild(contenedorMovie);
+
+    });
+}
+datosMovies();
--- public/services/servicesMovies.js
+++ public/services/servicesMovies.js
@@ -1,24 +1,47 @@
 async function getMovies() {
     try {
-        
         const response = await fetch ('http://localhost:3001/peliculas',{
            
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             }
-        })
+        });
 
-        //espere ese response y conviertala en .json
-        const users = await response.json()
+        //espera ese response y conviertala en .json
+        const movies = await response.json()
 
-        return users
+        return movies;
 
     } catch (error) {
-        console.error("Hay un error al obtener la pelicula", error)
+        console.error("Hay un error al obtener la pelicula", error);
+
+        throw error;
+    }
+}
+
+async function postMovies(movie) {
+    try {
+        
+        const response = await fetch ('http://localhost:3001/peliculas',{
+           
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body:JSON.stringify(movie)
+        })
+
+        const movies = await response.json();
+
+        return movies;
+
+    } catch (error) {
+        console.error("Hay un error al crear la pelicula", error);
 
         throw error
     }
 }
 
-export{getMovies}
+export{getMovies, postMovies};
